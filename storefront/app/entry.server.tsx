92fc2f043496169956a1f72b9faa61b563035ca0
hoyee0218-sys <hoyee0218@gmail.com>
2026-07-20 21:41:44 +0200

    fix: update to fix deployment error

--- storefront/app/entry.server.tsx
+++ storefront/app/entry.server.tsx
@@ -1,20 +1,18 @@
-import {ServerRouter} from 'react-router';
-import {isbot} from 'isbot';
-import {renderToReadableStream} from 'react-dom/server';
+import { handleRequest } from '@vercel/react-router/entry.server';
 import {
   createContentSecurityPolicy,
   type HydrogenRouterContextProvider,
 } from '@shopify/hydrogen';
-import type {EntryContext} from 'react-router';
+import type { AppLoadContext, EntryContext } from 'react-router';
 
-export default async function handleRequest(
+export default async function (
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   reactRouterContext: EntryContext,
   context: HydrogenRouterContextProvider,
 ) {
-  const {nonce, header, NonceProvider} = createContentSecurityPolicy({
+  const { nonce, header } = createContentSecurityPolicy({
     shop: {
       checkoutDomain: context.env.PUBLIC_CHECKOUT_DOMAIN,
       storeDomain: context.env.PUBLIC_STORE_DOMAIN,
@@ -35,33 +33,16 @@ export default async function handleRequest(
     ],
   });
 
-  const body = await renderToReadableStream(
-    <NonceProvider>
-      <ServerRouter
-        context={reactRouterContext}
-        url={request.url}
-        nonce={nonce}
-      />
-    </NonceProvider>,
-    {
-      nonce,
-      signal: request.signal,
-      onError(error) {
-        console.error(error);
-        responseStatusCode = 500;
-      },
-    },
+  const response = await handleRequest(
+    request,
+    responseStatusCode,
+    responseHeaders,
+    reactRouterContext,
+    context as unknown as AppLoadContext,
+    { nonce },
   );
 
-  if (isbot(request.headers.get('user-agent'))) {
-    await body.allReady;
-  }
+  response.headers.set('Content-Security-Policy', header);
 
-  responseHeaders.set('Content-Type', 'text/html');
-  responseHeaders.set('Content-Security-Policy', header);
-
-  return new Response(body, {
-    headers: responseHeaders,
-    status: responseStatusCode,
-  });
+  return response;
 }
